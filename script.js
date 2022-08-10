f781f2ab03eae115dbed936fb984a00556d0c31d
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-10 17:02:44 -0600

    strings

--- script.js
+++ script.js
@@ -31,31 +31,45 @@
 
 // var job = "programmer";
 // job = "coder";
-const now = 2022;
-const ageAhmed = now - 1988;
-const ageAtyaf = now - 1995;
-console.log(ageAhmed, ageAtyaf);
-console.log(ageAhmed * 2, ageAtyaf / 10, 9 ** 3);
-// ** is the power of operator 
+// const now = 2022;
+// const ageAhmed = now - 1988;
+// const ageAtyaf = now - 1995;
+// console.log(ageAhmed, ageAtyaf);
+// console.log(ageAhmed * 2, ageAtyaf / 10, 9 ** 3);
+// // ** is the power of operator 
+// const firstName = "Ahmed";
+// const lastName = "Hussein";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// // x = x + 10; 
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
+
+// console.log(ageAhmed > ageAtyaf);
+// console.log(ageAhmed === ageAhmed);
+
+// let z, y;
+// z = y = 25 - 10 - 5;
+// console.log(z > y);
+// let avarage = (ageAhmed + ageAtyaf) / 2;
+// console.log(avarage);
+
 const firstName = "Ahmed";
-const lastName = "Hussein";
-console.log(firstName + " " + lastName);
+const job = "Web developer";
+const birthYear = 1988;
+let year = 2022;
+const age = year - birthYear;
+const ahmed = "I'm " + firstName + ", a " + age + " years old " + job + "!";
 
-let x = 10 + 5;
-x += 10;
-// x = x + 10; 
-console.log(x);
-x++;
-console.log(x);
-x--;
-console.log(x);
+const ahmedTemplateLetteral = `I'm ${firstName}, a ${age} years old ${job}!`
+console.log(ahmedTemplateLetteral);
 
-console.log(ageAhmed > ageAtyaf);
-console.log(ageAhmed === ageAhmed);
-
-let z, y;
-z = y = 25 - 10 - 5;
-console.log(z > y);
-let avarage = (ageAhmed + ageAtyaf) / 2;
-console.log(avarage);
+console.log(ahmed);
+console.log(`hello, it's me!
+ Who are you?`)
 
