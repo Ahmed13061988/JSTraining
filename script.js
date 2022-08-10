798ac972eaf46084c294b01f9803d99f4a5110ea
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-10 09:46:18 -0600

    first

---
+++ script.js
@@ -0,0 +1,37 @@
+// // let js = 'amazing';
+// // console.log(40 + 8 + 23 - 10);
+
+
+// // console.log(23);
+
+// // let firstName = "Ahmed";
+// // console.log(firstName);
+
+// let country = "Iraq";
+// let continent = "Asia";
+// let population = 45000000;
+
+// // console.log(country, continent, population);
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof country);
+// console.log(typeof population);
+
+// javaScriptIsFun = 'YES!';
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// console.log(typeof null);
+
+// const birthDate = 1988;
+
+// var job = "programmer";
+// job = "coder";
+const now = 2022;
+const ageAhmed = now - 1988;
+const ageAtyaf = now - 1995;
+console.log(ageAhmed, ageAtyaf); 
