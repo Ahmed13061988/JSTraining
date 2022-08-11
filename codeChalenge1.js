b1755473e4387a6f7b9af339935d5c7732ab6258
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-10 23:19:17 -0600

    adding if statement to the chalenge

--- codeChalenge1.js
+++ codeChalenge1.js
@@ -6,15 +6,15 @@ const johnWeight = 92;
 
 let markHigherBMI;
 
-let markBMI = markWeight / markHight ** 2;
+let markBMI = Math.floor(markWeight / markHight ** 2);
 
-let johnBMI = johnWeight / johnHight ** 2;
+let johnBMI = Math.floor(johnWeight / johnHight ** 2);
 console.log(markBMI, johnBMI);
 
 if (markBMI > johnBMI) {
-    console.log(markHigherBMI = true);
+    console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
 } else {
-    console.log(markHigherBMI = false);
+    console.log(`John's ${johnBMI}  is higher than Mark's BMI ${markBMI}!`);
 }
 
 // const markHight = 1.88;
--- ifStatement.js
+++ ifStatement.js
@@ -8,3 +8,12 @@ if (age >= 18) {
     const yearsLeft = 18 - age;
     console.log(`Sarah is too young, wait another ${yearsLeft} years!`)
 }
+
+const birthYear = 2001;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century); 
