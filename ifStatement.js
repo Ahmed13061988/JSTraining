48d40e2c98b088179b47633466e8ab18006a5624
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-10 23:06:17 -0600

    adding if  else block

---
+++ ifStatement.js
@@ -0,0 +1,10 @@
+const age = 15;
+
+
+
+if (age >= 18) {
+    console.log(`Sarah can start driver license 🚗`);
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young, wait another ${yearsLeft} years!`)
+}
